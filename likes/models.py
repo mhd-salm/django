25f853cd942f4ecb9a3134a590c9390979a0b776
salman <103653847+mhd-salm@users.noreply.github.com>
2026-06-11 13:25:08 +0530

    fafa

--- likes/models.py
+++ likes/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-class Tagged_item(models.Model):
-    #what tag is connect to which object
-    tag = models.ForeignKey(Tag,on_delete=models.CASCADE)
-    #type
-    #id
+class Liked_items(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType,on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     object_content = GenericForeignKey()
