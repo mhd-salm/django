827210730b8083fd65c428a52ea2c0609c5ad1e4
salman <103653847+mhd-salm@users.noreply.github.com>
2026-06-13 22:13:50 +0530

    edas

--- playground/views.py
+++ playground/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q,F
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product,OrderItem
 
+                                                               
 
 def sup(request):
 
     #product = Product.objects.filter(Q(inventory__lt=10) | Q(price__gt=20))
 
-    product = Product.objects.filter()
-    
-    return render(request,'hello.html',{'name':'salman','prices': list(product)})
+    items = Product.objects.filter(id__in=OrderItem.objects.values_list('product_id').distinct()).order_by('title')
+
+
+    itemsa = Product.objects.filter(id__in=OrderItem.objects.values('product_id')).distinct().order_by('title') 
+    product = Product.objects.order_by('title')
+    return render(request,'hello.html',{'name':'salman','prices': list(itemsa)})
